004986725da583e8448b78794336f069f9efb167
init1 <28729137+0xinit@users.noreply.github.com>
2023-04-20 00:26:30 -0500

    chore: added a few things

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,9 +1,15 @@
 import styles from "../styles/Home.module.css";
+import TokenGated from "../components/TokenGated";
+import TestTokenGated from "../components/TestTokenGated";
+
 
 export default function Home() {
   return (
     <div>
       <main className={styles.main}>
+        {/* <TokenGated></TokenGated> */}
+        <TestTokenGated />
+
       </main>
     </div>
   );
