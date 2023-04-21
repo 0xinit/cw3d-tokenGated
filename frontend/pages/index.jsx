0f014b6ade777949beabba54233d06a1fc9db196
init1 <28729137+0xinit@users.noreply.github.com>
2023-04-21 02:42:58 -0500

    style: removed unwanted codebase and import of home css

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -1,13 +1,9 @@
-import styles from "../styles/Home.module.css";
 import TokenGated from "../components/TokenGated";
 
 export default function Home() {
   return (
     <div>
-      {/* <main className={styles.main}> */}
       <main>
-        {/* <TokenGated></TokenGated> */}
-        {/* <TestTokenGated /> */}
         <TokenGated />
       </main>
     </div>
