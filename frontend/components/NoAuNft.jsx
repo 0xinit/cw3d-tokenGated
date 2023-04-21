3d21d8fc9717529615bcbd1e4d0e37e16bef4d32
init1 <28729137+0xinit@users.noreply.github.com>
2023-04-21 00:31:12 -0500

    style: added emojis to make it look nice

--- frontend/components/NoAuNft.jsx
+++ frontend/components/NoAuNft.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styles from '../styles/NoAuNft.module.css';
 
 const NoAuNft = ({ walletAddress, contractAddress }) => {
+  const sadFace = "\u{1F622}";
   return (
     <div className={styles.noAccessContainer}>
       <div className={styles.noAccessBox}>
-        <h1 className={styles.noAccessTitle}>No Alchemy University Early Access NFT</h1>
+        <h1 className={styles.noAccessTitle}>No Alchemy University Early Access NFT {sadFace}</h1>
         <p className={styles.noAccessText}>
           Your wallet <span className={styles.addressText}>{walletAddress}</span> does not own an Alchemy University Early Access NFT from the contract <span className={styles.addressText}>{contractAddress}</span>, and that's why you are ineligible to mint the CW3D NFT or any Alchemy DAO Token.
         </p>
--- frontend/components/RestrictedAccess.jsx
+++ frontend/components/RestrictedAccess.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import styles from '../styles/RestrictedAccess.module.css';
 
 const RestrictedAccess = () => {
+    const blueHeart = "\u{1F499}";
     return (
         <div className={styles.restrictedContainer}>
             <div className={styles.restrictedBox}>
                 <h1 className={styles.restrictedTitle}>Restricted Page</h1>
                 <p className={styles.restrictedText}>
-                    The dapp you're trying to access to mint an NFT is token-gated,
-                    and you need to have an Alchemy Early access NFT to mint a CW3D NFT.
+                    The minting page you're trying to access to mint an NFT is token-gated,
+                    and you need to have an Alchemy Early access NFT to mint a CW3D NFT. Please connect your wallet to verify your ownership. PLW3 {blueHeart}
                 </p>
             </div>
         </div>
