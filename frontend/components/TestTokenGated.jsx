40f7ad4b46ea86c38879d56547114b70e2b36efe
init1 <28729137+0xinit@users.noreply.github.com>
2023-04-20 20:48:58 -0500

    fix: fixed the hydration error

--- frontend/components/TestTokenGated.jsx
+++ frontend/components/TestTokenGated.jsx
@@ -1,35 +1,49 @@
 // TestTokenGated.jsx
-import React from "react";
+import React, { useEffect, useState } from "react";
 import TokenGated from "./TokenGated";
+import RestrictedAccess from "./RestrictedAccess";
 import { useAccount } from "wagmi";
 
 function TestTokenGated() {
-    const walletAddress = "0x8480D20583A3138Fef7C23EeD8F17Bf3C01E73b7"; // 
-    // const { walletAddress, isConnected, isDisconnected } = useAccount();
-    console.log(walletAddress)
-    // Replace with a valid wallet address
-    const contractAddress = "0x60576A64851C5B42e8c57E3E4A5cF3CF4eEb2ED6"; // Replace with a valid contract address
-    const chain = "MATIC_MAINNET"; // You can change this to another chain if needed
+    const { address: walletAddress, isConnected, isDisconnected } = useAccount();
+
+    // Replace with any contract address
+    const contractAddress = "0x60576A64851C5B42e8c57E3E4A5cF3CF4eEb2ED6";
+    // You can change this to another chain if needed
+    const chain = "MATIC_MAINNET";
 
     const { isNftOwner, isLoading } = TokenGated({ walletAddress, contractAddress, chain });
-    // console.log(walletAddress)
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+        return () => {
+            setIsMounted(false);
+        };
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
+
     if (isLoading) {
         return <div>Loading...</div>;
     }
-    // console.log(isNftOwner)
-
-    if (isNftOwner) {
-        return (
-            <div>
-                The wallet address {walletAddress} owns the NFT from the contract address {contractAddress}!
-            </div>
-        );
-    }
-
     return (
-        <div>
-            The wallet address {walletAddress} does not own the NFT from the contract address {contractAddress}.
-        </div>
+        <>
+            {isDisconnected ? (
+                <RestrictedAccess />
+            ) : isNftOwner ? (
+                <div>
+                    The wallet address {walletAddress} owns the NFT from the contract
+                    address {contractAddress}!
+                </div>
+            ) : isLoading ? (<div>loading...</div>) : (<div>
+                The wallet address {walletAddress} does not own the NFT from the contract address {contractAddress}.
+            </div>)
+
+            }
+        </>
     );
 }
 
