9fd11c7001b15524bec32ef6f5d7a11d486d9719
init1 <28729137+0xinit@users.noreply.github.com>
2023-04-21 02:29:39 -0500

    refactor: removed unnecessary code from Dao Minter

--- frontend/components/AlchemyDaoMinter.jsx
+++ frontend/components/AlchemyDaoMinter.jsx
@@ -6,19 +6,13 @@ const AlchemyDaoMinter = ({ walletAddress }) => {
 
     const handleMint = async () => {
         setIsMinting(true);
-        // Call your ERC20 contract with governance functionality to mint 500 DAO tokens here
-        // Remember to set setIsMinting(false) after the minting process is completed or if an error occurs
+        // Call ERC20 here, we will leave as it is for a while.
     };
 
     return (
         <div className={styles.container}>
             <h2>Mint Alchemy DAO Token</h2>
             <div>
-                <img
-                    src="your_blue_svg_image_url_here"
-                    alt="blue_svg"
-                    style={{ width: "100px", height: "100px" }}
-                />
                 <p>You will be able to mint 500 DAO tokens.</p>
                 <p>Connected Wallet Address: {walletAddress}</p>
                 <button onClick={handleMint} disabled={isMinting} className={styles.button}>
